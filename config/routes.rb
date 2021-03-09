09bc87f3935a846d7133201acd5926fa28b866e2
TakKokubu <kokubu.tk@gmail.com>
2021-03-09 23:00:21 +0900

    userログイン機能　controller作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'users/new'
+  get 'users/create'
+  get 'users/update'
+  get 'users/edit'
   root to: 'toppages#index'
 
   get 'signup', to: 'users#new'
