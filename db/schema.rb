041ea22c88cf32bce8d4fc9079d094465d9fa981
TakKokubu <kokubu.tk@gmail.com>
2021-03-17 17:09:40 +0900

    user登録時のprefecture項目のDBへの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_09_063100) do
+ActiveRecord::Schema.define(version: 2021_03_17_080559) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2021_03_09_063100) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "prefecture"
   end
 
 end
