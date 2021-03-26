f695caac957ee7382b189e0454987ee50b84a2ff
TakKokubu <kokubu.tk@gmail.com>
2021-03-26 21:36:40 +0900

    seedファイル変更

--- db/schema.rb
+++ db/schema.rb
@@ -17,18 +17,18 @@ ActiveRecord::Schema.define(version: 2021_03_24_093751) do
 
   create_table "prefectures", force: :cascade do |t|
     t.string "name"
-    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_prefectures_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.string "password_digest"
+    t.integer "prefecture_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["prefecture_id"], name: "index_users_on_prefecture_id"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,35 +5,60 @@ User.create!(
       name: "テスト1",
       password: "123456",
       password_confirmation: "123456",
-      prefecture: "東京"
+      prefecture_id: 1
     },
     {
       email: "test2@test.com",
       name: "テスト2",
       password: "123456",
       password_confirmation: "123456",
-      prefecture: "埼玉"
+      prefecture_id: 2
     },
     {
       email: "test3@test.com",
       name: "テスト3",
       password: "123456",
       password_confirmation: "123456",
-      prefecture: "神奈川"
+      prefecture_id: 3
     },
     {
       email: "test4@test.com",
       name: "テスト4",
       password: "123456",
       password_confirmation: "123456",
-      prefecture: "千葉"
+      prefecture_id: 4
     },
     {
       email: "test5@test.com",
       name: "テスト5",
       password: "123456",
       password_confirmation: "123456",
-      prefecture: "茨城"
+      prefecture_id: 5
+    }
+  ]
+)
+
+Prefecture.create! (
+  [
+    {
+      id: 1,
+      name: "茨城"
+    },
+    {
+      id: 2,
+      name: "埼玉"
+    },
+    {
+      id: 3,
+      name: "千葉"
+    },
+    {
+      id: 4,
+      name: "東京"
+    },
+    {
+      id: 5,
+      name: "神奈川"
     }
   ]
 )
