3f3d141bb5f4b027cd8d083b1293bedc143bcfbd
TakKokubu <kokubu.tk@gmail.com>
2021-04-03 23:43:35 +0900

    modelの作成

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,3 +1,8 @@
 class Spot < ApplicationRecord
   belongs_to :walkcourse
+  validates :name, length: { maximum: 50 }
+  validates :address, length: { maximum: 50 }
+  validates :description, length: { maximum: 250 }
+  # transit_time
+  # time_required
 end
--- app/models/walkcourse.rb
+++ app/models/walkcourse.rb
@@ -1,3 +1,6 @@
 class Walkcourse < ApplicationRecord
   belongs_to :user
+  has_many :spots, dependent: :destroy
+  validates :title, presence: true, length: { maximum: 50 }
+  validates :description, presence: true, length: { maximum: 255 }
 end
