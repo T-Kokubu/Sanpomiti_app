9d02989c9ca289c8fa4e4bd0523fe2b5440105ad
TakKokubu <kokubu.tk@gmail.com>
2021-04-04 12:45:05 +0900

    model修正

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,8 +1,12 @@
 class Spot < ApplicationRecord
+  before_save do
+    transit_time[0] = nil unless nil
+    time_required[0] = nil unless nil
+    time_required[-1] = nil unless nil
+  end
+
   belongs_to :walkcourse
   validates :name, length: { maximum: 50 }
   validates :address, length: { maximum: 50 }
   validates :description, length: { maximum: 250 }
-  # transit_time
-  # time_required
 end
