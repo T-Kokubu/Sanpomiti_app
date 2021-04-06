ec2ad806bdb2011d42aa2f0d41bfc2ecded27939
TakKokubu <kokubu.tk@gmail.com>
2021-04-06 11:30:13 +0900

    walkcourse controller一部作成

--- app/controllers/walkcourses_controller.rb
+++ app/controllers/walkcourses_controller.rb
@@ -3,9 +3,19 @@ class WalkcoursesController < ApplicationController
   end
 
   def new
+    @walkcourse = Walkcourse.new
   end
 
   def create
+    @walkcourse = Walkcourse.new(walkcourse_params)
+
+    if @walkcourse.save
+      flash[:success] = 'コースが登録されました。'
+      redirect_to edit_walkcourse_path(@walkcourse)
+    else
+      flash.now[:danger] = 'コースの登録に失敗しました。'
+      render 'new'
+    end
   end
 
   def show
--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,12 +1,28 @@
 class Spot < ApplicationRecord
-  before_save do
-    transit_time[0] = nil unless nil
-    time_required[0] = nil unless nil
-    time_required[-1] = nil unless nil
-  end
 
   belongs_to :walkcourse
   validates :name, length: { maximum: 50 }
   validates :address, length: { maximum: 50 }
   validates :description, length: { maximum: 250 }
+
+  before_save do
+    # ここでbinding.pryで止めてselfとはなんなのか確認してみましょう！
+    binding.pry
+    # selfといのは、Spotオブジェクトになります。
+    # 例えば、Spot.create(id: 1, name: '町田駅')というspotがあった場合に
+    # Spot.find(1).update(name: '新宿駅')
+    # とやった場合、before_saveの中を通ります。
+    # このときselfが指すのはSpot.find(1)のオブジェクトになります。
+
+    # 自身が属するWalkcourseが持っているspotの中で最初の場合
+    if self.walkcourse.spots.first == self
+      self.transit_time = nil
+      self.time_required = nil
+    end
+
+    # 自身が属するWalkcourseが持っているspotの中で最後の場合
+    if self.walkcourse.spots.last == self
+      self.time_required = nil
+    end
+  end
 end
