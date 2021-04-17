55719faeb0550c16702f4e18d63264666909e41a
TakKokubu <kokubu.tk@gmail.com>
2021-04-17 15:24:11 +0900

    spot controller editの修正

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -5,13 +5,13 @@ class SpotsController < ApplicationController
   end
 
   def create
-    @walkcourse = Walkcourse.find(params[:id])
+    @walkcourse = Walkcourse.find(params[:walkcourse_id])
     @spot = @walkcourse.spots.build
 
     if @spot.save
-      binding.pry
       flash[:success] = 'スポットが登録されました。'
       redirect_to edit_walkcourse_spot_path(@walkcourse, @spot)
+
     else
       flash.now[:danger] = 'スポットの登録に失敗しました。'
       redirect_to new_walkcourse_path
@@ -22,11 +22,14 @@ class SpotsController < ApplicationController
 
   def edit
     @walkcourse = Walkcourse.find(params[:walkcourse_id])
-    @spot = @walkcourse.spots.find(params[:id])
+    @spot = Spot.find(params[:id])
+
   end
 
   def update
     @spot = Spot.find(params[:id])
+    @walkcourse = Walkcourse.find(params[:walkcourse_id])
+    
     if @spot.update_attributes(spot_params)
       # 更新に成功したときの処理
       flash[:success] = 'スポットの更新に成功しました。'
