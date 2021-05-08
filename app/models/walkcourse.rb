e850744d7e46f688f3445fee45543a52e2198014
TakKokubu <kokubu.tk@gmail.com>
2021-05-09 00:57:53 +0900

    rubocop修正

--- app/models/walkcourse.rb
+++ app/models/walkcourse.rb
@@ -6,6 +6,6 @@ class Walkcourse < ApplicationRecord
 
   validates :title, presence: true, length: { maximum: 50 }
   validates :description, presence: true, length: { maximum: 255 }
-  validates :time_to_first_spot,format: { with: /\A[0-9]+\z/ }
+  validates :time_to_first_spot, format: { with: /\A[0-9]+\z/ }
   mount_uploader :coursepic, CoursepicUploader
 end
