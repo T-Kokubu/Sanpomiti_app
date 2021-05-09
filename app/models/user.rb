b84eaca8c7bd5e73561b79db532e1e3d118ca8d3
TakKokubu <kokubu.tk@gmail.com>
2021-05-09 23:17:54 +0900

    favorite関係のmodel作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,27 @@ class User < ApplicationRecord
   # prefectureに属する
   belongs_to :prefecture
   has_many :walkcourses, dependent: :destroy
+
+
+  has_many :favorites
+  has_many :likes, through: :favorites, source: :like
+  has_many :reverses_of_favorite, class_name: "Favorite", foreign_key: "like_id"
+  has_many :subjects, through: :reverses_of_favorite, source: :user
+
+  def like(like_walkcourse)
+      self.favorites.find_or_create_by(like_id: like_walkcourse.id)
+  end
+
+  def unlike(like_walkcourse)
+    favorite = self.favorites.find_by(like_id: like_walkcourse.id)
+    favorite.destroy if favorite
+  end
+
+  def liking?(like_walkcourse)
+    self.likes.include?(like_walkcourse)
+  end
+
+  def feed_favorites
+    Walkcourse.where(id: self.like_ids)
+  end
 end
