439c26057068dadbced6b38e190726c328639179
TakKokubu <kokubu.tk@gmail.com>
2021-05-09 23:27:53 +0900

    favorite関係のrouteの作成

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,16 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
-
   get 'signup', to: 'users#new'
-  resources :users, only: [:show, :create, :edit, :update]
+
   resources :walkcourses do
     resources :spots, only: [:new, :create, :edit, :update, :destroy]
   end
+  resources :users, only: [:show, :create, :edit] do
+    member do
+      get :likes
+      get :subjects
+    end
+  end
+  resources :favorites, only: [:create, :destroy]
 end
