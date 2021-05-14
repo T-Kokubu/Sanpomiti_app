e0d583b25511b376398172bbf24907d74fd05c45
TakKokubu <kokubu.tk@gmail.com>
2021-05-14 22:05:55 +0900

    toppage_controllerの修正

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,6 +1,6 @@
 class ToppagesController < ApplicationController
   def index
-    @created_walkcourses = Walkcourse.all.order(created_at: :desc)
-    @favorite_walkcourses = Walkcourse.joins(:favorites).group(:like_id).order('count(favorites.user_id) desc')
+    @created_walkcourses = Walkcourse.all.order(created_at: :asc).page(params[:page]).per(6)
+    @favorite_walkcourses = Walkcourse.joins(:favorites).group('walkcourses.id', :like_id).order('count(favorites.user_id) desc').page(params[:page]).per(6)
   end
 end
