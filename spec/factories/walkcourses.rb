6cf8629f47e92229263fbadda81b9b84f16e42a7
TakKokubu <kokubu.tk@gmail.com>
2021-05-19 23:35:59 +0900

    walkcourse model rspecテスト

--- spec/factories/walkcourses.rb
+++ spec/factories/walkcourses.rb
@@ -2,6 +2,9 @@ FactoryBot.define do
   factory :walkcourse do
     title { "rspectest" }
     description { "rspectest" }
+    time_to_first_spot { 1 }
+    start_station { "東京" }
+    goal_station { "有楽町" }
   end
   factory :otherwalkcourse, class:  Walkcourse do
     title { "otherrspectest" }
--- spec/models/walkcourse_spec.rb
+++ spec/models/walkcourse_spec.rb
@@ -1,46 +1,64 @@
 require 'rails_helper'
 
 RSpec.describe Walkcourse, type: :model do
+  let!(:user) { create(:user) }
+  let!(:walkcourse) { create(:walkcourse, user: user) }
+
   describe 'titleについて' do
-    it '規定文字数の入力があれば有効' do
-    end
     it '入力がなければエラー表示する' do
+      walkcourse = build(:walkcourse, title: '')
+      walkcourse.valid?
+      expect(walkcourse.errors[:title]).to include('を入力してください')
     end
     it '規定文字数を超える入力はエラー表示' do
+      walkcourse = build(:walkcourse, title: 'a' * 51)
+      walkcourse.valid?
+      expect(walkcourse.errors[:title]).to include('は50文字以内で入力してください')
     end
   end
 
   describe 'descriptionについて' do
-    it '規定文字数の入力があれば有効' do
-    end
     it '入力がなければエラー表示する' do
+      walkcourse = build(:walkcourse, description: '')
+      walkcourse.valid?
+      expect(walkcourse.errors[:description]).to include('を入力してください')
     end
     it '規定文字数を超える入力はエラー表示' do
+      walkcourse = build(:walkcourse, description: 'a' * 256)
+      walkcourse.valid?
+      expect(walkcourse.errors[:description]).to include('は255文字以内で入力してください')
     end
   end
 
   describe 'time_to_first_spot' do
-    it '整数の入力は有効になる' do
-    end
     it '入力がない場合でも有効' do
+      walkcourse = build(:walkcourse, time_to_first_spot: '', user: user)
+      expect(walkcourse).to be_valid
     end
     it '整数以外の入力はエラー表示する' do
-    end
-    it '規定文字数を超える入力はエラー表示' do
+      walkcourse = build(:walkcourse, time_to_first_spot: 'あ', user: user)
+      walkcourse.valid?
+      expect(walkcourse.errors[:time_to_first_spot]).to include('は数値で入力してください')
     end
   end
 
   describe 'start_station' do
-    it '規定文字数内の入力は有効になる' do
-    end
     it '規定文字数を超える入力はエラー' do
+      walkcourse = build(:walkcourse, start_station: 'a' * 21)
+      walkcourse.valid?
+      expect(walkcourse.errors[:start_station]).to include('は20文字以内で入力してください')
     end
   end
 
   describe 'goal_station' do
-    it '規定文字数内の入力は有効になる' do
-    end
     it '規定文字数を超える入力はエラー' do
+      walkcourse = build(:walkcourse, goal_station: 'a' * 21)
+      walkcourse.valid?
+      expect(walkcourse.errors[:goal_station]).to include('は20文字以内で入力してください')
     end
+  end
+
+  it '規定文字数の入力があれば有効' do
+    expect(walkcourse).to be_valid
   end
 end
