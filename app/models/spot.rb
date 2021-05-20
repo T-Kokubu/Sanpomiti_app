cd4f38025398c8d919826f3dbe7aaee8aa2d4084
TakKokubu <kokubu.tk@gmail.com>
2021-05-20 23:06:01 +0900

    spot model rspecテスト追加

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,7 +1,7 @@
 class Spot < ApplicationRecord
   belongs_to :walkcourse
-  validates :name, length: { maximum: 50 }
-  validates :address, length: { maximum: 50 }
+  validates :name, length: { maximum: 20 }
+  validates :address, length: { maximum: 30 }
   validates :description, length: { maximum: 250 }
   validates :transit_time, numericality: { only_integer: true }, allow_blank: true
   validates :time_required, numericality: { only_integer: true }, allow_blank: true
--- spec/models/spot_spec.rb
+++ spec/models/spot_spec.rb
@@ -3,21 +3,53 @@ require 'rails_helper'
 RSpec.describe Spot, type: :model do
   let!(:user) { create(:user) }
   let!(:walkcourse) { create(:walkcourse, user: user) }
-  let!(:spot) { create(:spot, user: user, walkcourse: walkcourse) }
+  let!(:spot) { create(:spot, walkcourse: walkcourse) }
+
+  it '規定文字数の入力があれば有効' do
+    expect(spot).to be_valid
+  end
 
   describe 'name' do
-
+    it '規定文字数を超える入力はエラー' do
+      spot = build(:spot, name: 'a' * 21)
+      spot.valid?
+      expect(spot.errors[:name]).to include('は20文字以内で入力してください')
+    end
   end
   describe 'address' do
-
+    it '規定文字数を超える入力はエラー' do
+      spot = build(:spot, address: 'a' * 31)
+      spot.valid?
+      expect(spot.errors[:address]).to include('は30文字以内で入力してください')
+    end
   end
   describe 'description' do
-
+    it '規定文字数を超える入力はエラー' do
+      spot = build(:spot, description: 'a' * 251)
+      spot.valid?
+      expect(spot.errors[:description]).to include('は250文字以内で入力してください')
+    end
   end
   describe 'transit_time' do
-
+    it '入力がない場合でも有効' do
+      spot = build(:spot, transit_time: '', walkcourse: walkcourse)
+      expect(spot).to be_valid
+    end
+    it '整数以外の入力はエラー表示する' do
+      spot = build(:spot, transit_time: '/[^0-9]/', walkcourse: walkcourse)
+      spot.valid?
+      expect(spot.errors[:transit_time]).to include('は数値で入力してください')
+    end
   end
   describe 'time_required' do
-
+    it '入力がない場合でも有効' do
+      spot = build(:spot, time_required: '', walkcourse: walkcourse)
+      expect(spot).to be_valid
+    end
+    it '整数以外の入力はエラー表示する' do
+      spot = build(:spot, time_required: '/[^0-9]/', walkcourse: walkcourse)
+      spot.valid?
+      expect(spot.errors[:time_required]).to include('は数値で入力してください')
+    end
   end
 end
