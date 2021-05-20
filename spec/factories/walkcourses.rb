24e145d5966ccd20e31bf0fc6fea8bc34fbbca9d
TakKokubu <kokubu.tk@gmail.com>
2021-05-20 22:07:27 +0900

    walkcourse model rspecテスト

--- spec/factories/walkcourses.rb
+++ spec/factories/walkcourses.rb
@@ -5,6 +5,7 @@ FactoryBot.define do
     time_to_first_spot { 1 }
     start_station { "東京" }
     goal_station { "有楽町" }
+    coursepic { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/image/monster11.png')) }
   end
   factory :otherwalkcourse, class:  Walkcourse do
     title { "otherrspectest" }
